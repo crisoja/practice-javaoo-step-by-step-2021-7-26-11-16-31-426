398cfea3c32cdeceb39ff8e1137f2a2e58d0b6ef
Janley Crsiologo <crisoja@oocl.com>
2021-08-04 21:30:31 +0800

    [Practice03][Crisologo_Janley] feat: Implement OOP and logics

--- src/main/java/practice03/Student.java
+++ src/main/java/practice03/Student.java
@@ -1,4 +1,31 @@
 package practice03;
 
-public class Student {
+public class Student extends Person{
+    private String name;
+    private int age;
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name,age);
+        this.name = name;
+        this.age = age;
+        this.klass = klass;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    @Override
+    public String introduce(){
+        return  String.format("I am a Student. I am at Class %d.",klass);
+    }
 }
--- src/main/java/practice03/Worker.java
+++ src/main/java/practice03/Worker.java
@@ -1,4 +1,25 @@
 package practice03;
 
-public class Worker {
+public class Worker extends Person{
+    private String name;
+    private int age;
+
+    public Worker(String name, int age) {
+        super(name,age);
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    @Override
+    public String introduce(){
+        return  String.format("I am a Worker. I have a job.");
+    }
 }
